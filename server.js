c316fe9a2790b828fd610b8c381e794a04cd56a8
HarshSharma0801 <121893196+HarshSharma0801@users.noreply.github.com>
2023-06-17 22:07:15 +0530

    update

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ const app = express();
 
 app.get('/',(req,res)=>{
 res.send('server is up and running');
+const url = 'https://api.weatherapi.com/v1/current.json?key=2fe72bce856b428bb27204334231606&q=London&aqi=no';
+https.get(url,function(response){
+    console.log(response.statusCode);
+});
 
 })
 
